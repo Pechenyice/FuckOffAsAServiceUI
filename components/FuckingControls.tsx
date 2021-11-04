55371930937d7e412772d271e8266c04a90c19af
Pechenyice <german.iskhakov@mail.ru>
2021-11-05 02:43:24 +0300

    finished controls component

--- components/FuckingControls.tsx
+++ components/FuckingControls.tsx
@@ -2,23 +2,28 @@
 import { IFuckingControlsProps } from '../types/commonTypes';
 import styles from './../styles/FuckingControls.module.css';
 
-export default function FuckingCard({onChange, onNewView, currentView} : IFuckingControlsProps) {
+export default function FuckingCard({ onChange, onNewView, currentView }: IFuckingControlsProps) {
 
     function viewFactory(text: string): any {
-        return (): void  => {
+        return (): void => {
             onNewView(text)
         }
     }
 
     return (
         <div className={styles.wrapper}>
-            <input onChange={onChange} className={styles.input} type={'text'} placeholder='filter fucks' />
+            <div className={styles.inputsWrapper}>
+                <input onChange={onChange} className={styles.input} type={'text'} placeholder='filter fucks' />
+                <div className={styles.inputIcon}><i className="fas fa-search"></i></div>
+            </div>
             <div className={styles.switchesWrapper}>
                 <div className={[styles.switcher, currentView == 'ceil' && styles.activeSwitcher].join(' ')} onClick={viewFactory('ceil')}>
-                    &#9638;
+                    <p><i className="fas fa-table"></i></p>
+                    <p>table</p>
                 </div>
                 <div className={[styles.switcher, currentView == 'list' && styles.activeSwitcher].join(' ')} onClick={viewFactory('list')}>
-                    &#9636;
+                    <p><i className="fas fa-list-ul"></i></p>
+                    <p>list</p>
                 </div>
             </div>
         </div>
