e6815162d0dac78cb935bb2f10ce211ef0672dcf
Pechenyice <german.iskhakov@mail.ru>
2021-11-05 02:44:12 +0300

    added progress bar to app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import '../styles/globals.css';
 import { AppProps } from 'next/app'
+import NextNProgress from 'nextjs-progressbar';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <React.Fragment>
+      <NextNProgress
+        color="#014747"
+        startPosition={0.3}
+        stopDelayMs={200}
+        height={5}
+        showOnShallow={true}
+      />
       <Component {...pageProps} />
     </React.Fragment>
   );
